c99579d65d3aadbef07797f2f6e0be56e90e1b1a
kdvornichenko <gglade999@gmail.com>
2022-07-05 20:16:30 +0600

    Init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			boxShadow: {
+				wrapper: '0px 10px 20px rgba(0, 0, 0, 0.04)',
+			},
+			colors: {
+				grey: '#9D9D9D',
+				lightGreen: '#D6FFC8',
+				priceGrey: '#BDBDBD',
+			},
+		},
+	},
+	plugins: [],
+}
