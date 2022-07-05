5a66715c3785e1792bf3d0a2f399ff9a91a10e1e
kdvornichenko <gglade999@gmail.com>
2022-07-05 22:36:35 +0600

    Cards hover, favorite button

--- src/App.js
+++ src/App.js
@@ -75,16 +75,55 @@ function App() {
 					</li>
 				</ul>
 			</header>
-			<div className='content px-14 py-11'>
-				<h1 className='mb-10 text-4xl font-bold'>Все кроссовки</h1>
-
-				<div className='flex justify-between'>
-
-					<div className='card'>
-						<img
-							src='/img/sneakers/Nike-Blazer-Mid-Suede-Green.png'
-							alt='Nike Blazer Mid Suede Green'
+			<div className='px-14 py-11'>
+				<div className='mb-10 flex justify-between items-center'>
+					<h1 className='text-4xl font-bold'>Все кроссовки</h1>
+					<div className='search-block flex items-center gap-3 pl-3 border border-[#F3F3F3] rounded-xl'>
+						<svg
+							width='16'
+							height='16'
+							viewBox='0 0 16 16'
+							fill='none'
+							xmlns='http://www.w3.org/2000/svg'
+						>
+							<path
+								d='M15.25 15.25L11.8855 11.8795L15.25 15.25ZM13.75 7.375C13.75 9.06576 13.0784 10.6873 11.8828 11.8828C10.6873 13.0784 9.06576 13.75 7.375 13.75C5.68424 13.75 4.06274 13.0784 2.86719 11.8828C1.67165 10.6873 1 9.06576 1 7.375C1 5.68424 1.67165 4.06274 2.86719 2.86719C4.06274 1.67165 5.68424 1 7.375 1C9.06576 1 10.6873 1.67165 11.8828 2.86719C13.0784 4.06274 13.75 5.68424 13.75 7.375V7.375Z'
+								stroke='#E4E4E4'
+								stroke-width='2'
+								stroke-linecap='round'
+							/>
+						</svg>
+						<input
+							type='text'
+							placeholder='Поиск'
+							className='w-52 text-base outline-grey py-2 outline-0'
 						/>
+					</div>
+				</div>
+
+				<div className='flex justify-between flex-wrap gap-1'>
+					<div className='card'>
+						<div className='relative'>
+							<button className='card-heart'>
+								<svg
+									className='heart-svg transition-all'
+									width='17'
+									height='16'
+									viewBox='0 0 17 16'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M8.03078 14.9412C7.9457 14.8921 6.1608 13.8555 4.34706 12.2912C3.27016 11.3625 2.39709 10.43 1.76148 9.51732C0.939559 8.33717 0.493248 7.15535 0.500452 6.01161L0.500452 6.01159C0.508636 4.7198 0.971706 3.49482 1.81529 2.568L1.81529 2.56799C2.67463 1.62389 3.83337 1.09399 5.07586 1.09399C6.4213 1.09399 7.65502 1.72907 8.50039 2.75678C9.34576 1.72909 10.5795 1.09399 11.9249 1.09399C13.0982 1.09399 14.2112 1.57119 15.0586 2.42404C15.988 3.35925 16.5088 4.67132 16.5003 6.0235C16.493 7.16312 16.0399 8.34117 15.2169 9.51863C14.5798 10.4301 13.7084 11.3618 12.6359 12.2902L12.6359 12.2902C10.829 13.8541 9.05704 14.8906 8.97259 14.9397L8.97242 14.9398C8.82659 15.0245 8.66338 15.067 8.50036 15.067C8.33842 15.067 8.17616 15.0251 8.03078 14.9412ZM8.03078 14.9412C8.03071 14.9412 8.03065 14.9411 8.03058 14.9411L8.28066 14.5081L8.03078 14.9412Z'
+										stroke='#D1D1D1'
+									/>
+								</svg>
+							</button>
+							<img
+								src='/img/sneakers/Nike-Blazer-Mid-Suede-Green.png'
+								alt='Nike Blazer Mid Suede Green'
+							/>
+						</div>
 						<h5 className='card-sneakers-name'>
 							Мужские Кроссовки Nike Blazer Mid Suede
 						</h5>
@@ -98,10 +137,27 @@ function App() {
 					</div>
 
 					<div className='card'>
-						<img
-							src='/img/sneakers/Nike-Air-Max-270.png'
-							alt='Nike Air Max 270'
-						/>
+						<div className='relative'>
+							<button className='card-heart'>
+								<svg
+									className='heart-svg transition-all'
+									width='17'
+									height='16'
+									viewBox='0 0 17 16'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M8.03078 14.9412C7.9457 14.8921 6.1608 13.8555 4.34706 12.2912C3.27016 11.3625 2.39709 10.43 1.76148 9.51732C0.939559 8.33717 0.493248 7.15535 0.500452 6.01161L0.500452 6.01159C0.508636 4.7198 0.971706 3.49482 1.81529 2.568L1.81529 2.56799C2.67463 1.62389 3.83337 1.09399 5.07586 1.09399C6.4213 1.09399 7.65502 1.72907 8.50039 2.75678C9.34576 1.72909 10.5795 1.09399 11.9249 1.09399C13.0982 1.09399 14.2112 1.57119 15.0586 2.42404C15.988 3.35925 16.5088 4.67132 16.5003 6.0235C16.493 7.16312 16.0399 8.34117 15.2169 9.51863C14.5798 10.4301 13.7084 11.3618 12.6359 12.2902L12.6359 12.2902C10.829 13.8541 9.05704 14.8906 8.97259 14.9397L8.97242 14.9398C8.82659 15.0245 8.66338 15.067 8.50036 15.067C8.33842 15.067 8.17616 15.0251 8.03078 14.9412ZM8.03078 14.9412C8.03071 14.9412 8.03065 14.9411 8.03058 14.9411L8.28066 14.5081L8.03078 14.9412Z'
+										stroke='#D1D1D1'
+									/>
+								</svg>
+							</button>
+							<img
+								src='/img/sneakers/Nike-Air-Max-270.png'
+								alt='Nike Air Max 270'
+							/>
+						</div>
 						<h5 className='card-sneakers-name'>
 							Мужские Кроссовки Nike Air Max 270
 						</h5>
@@ -115,10 +171,27 @@ function App() {
 					</div>
 
 					<div className='card'>
-						<img
-							src='/img/sneakers/Nike-Blazer-Mid-Suede-White.png'
-							alt='NikeBlazer Mid Suede'
-						/>
+						<div className='relative'>
+							<button className='card-heart'>
+								<svg
+									className='heart-svg transition-all'
+									width='17'
+									height='16'
+									viewBox='0 0 17 16'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M8.03078 14.9412C7.9457 14.8921 6.1608 13.8555 4.34706 12.2912C3.27016 11.3625 2.39709 10.43 1.76148 9.51732C0.939559 8.33717 0.493248 7.15535 0.500452 6.01161L0.500452 6.01159C0.508636 4.7198 0.971706 3.49482 1.81529 2.568L1.81529 2.56799C2.67463 1.62389 3.83337 1.09399 5.07586 1.09399C6.4213 1.09399 7.65502 1.72907 8.50039 2.75678C9.34576 1.72909 10.5795 1.09399 11.9249 1.09399C13.0982 1.09399 14.2112 1.57119 15.0586 2.42404C15.988 3.35925 16.5088 4.67132 16.5003 6.0235C16.493 7.16312 16.0399 8.34117 15.2169 9.51863C14.5798 10.4301 13.7084 11.3618 12.6359 12.2902L12.6359 12.2902C10.829 13.8541 9.05704 14.8906 8.97259 14.9397L8.97242 14.9398C8.82659 15.0245 8.66338 15.067 8.50036 15.067C8.33842 15.067 8.17616 15.0251 8.03078 14.9412ZM8.03078 14.9412C8.03071 14.9412 8.03065 14.9411 8.03058 14.9411L8.28066 14.5081L8.03078 14.9412Z'
+										stroke='#D1D1D1'
+									/>
+								</svg>
+							</button>
+							<img
+								src='/img/sneakers/Nike-Blazer-Mid-Suede-White.png'
+								alt='NikeBlazer Mid Suede'
+							/>
+						</div>
 						<h5 className='card-sneakers-name'>
 							Мужские Кроссовки NikeBlazer Mid Suede
 						</h5>
@@ -132,10 +205,27 @@ function App() {
 					</div>
 
 					<div className='card'>
-						<img
-							src='/img/sneakers/Puma-X-Aka-Boku-Future-Rider.png'
-							alt='Puma X Aka Boku Future Rider'
-						/>
+						<div className='relative'>
+							<button className='card-heart'>
+								<svg
+									className='heart-svg transition-all'
+									width='17'
+									height='16'
+									viewBox='0 0 17 16'
+									fill='none'
+									xmlns='http://www.w3.org/2000/svg'
+								>
+									<path
+										d='M8.03078 14.9412C7.9457 14.8921 6.1608 13.8555 4.34706 12.2912C3.27016 11.3625 2.39709 10.43 1.76148 9.51732C0.939559 8.33717 0.493248 7.15535 0.500452 6.01161L0.500452 6.01159C0.508636 4.7198 0.971706 3.49482 1.81529 2.568L1.81529 2.56799C2.67463 1.62389 3.83337 1.09399 5.07586 1.09399C6.4213 1.09399 7.65502 1.72907 8.50039 2.75678C9.34576 1.72909 10.5795 1.09399 11.9249 1.09399C13.0982 1.09399 14.2112 1.57119 15.0586 2.42404C15.988 3.35925 16.5088 4.67132 16.5003 6.0235C16.493 7.16312 16.0399 8.34117 15.2169 9.51863C14.5798 10.4301 13.7084 11.3618 12.6359 12.2902L12.6359 12.2902C10.829 13.8541 9.05704 14.8906 8.97259 14.9397L8.97242 14.9398C8.82659 15.0245 8.66338 15.067 8.50036 15.067C8.33842 15.067 8.17616 15.0251 8.03078 14.9412ZM8.03078 14.9412C8.03071 14.9412 8.03065 14.9411 8.03058 14.9411L8.28066 14.5081L8.03078 14.9412Z'
+										stroke='#D1D1D1'
+									/>
+								</svg>
+							</button>
+							<img
+								src='/img/sneakers/Puma-X-Aka-Boku-Future-Rider.png'
+								alt='Puma X Aka Boku Future Rider'
+							/>
+						</div>
 						<h5 className='card-sneakers-name'>
 							Кроссовки Puma X Aka Boku Future Rider
 						</h5>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,12 @@ module.exports = {
 		extend: {
 			boxShadow: {
 				wrapper: '0px 10px 20px rgba(0, 0, 0, 0.04)',
+				card: '0px 14px 30px rgba(0, 0, 0, 0.05)',
 			},
 			colors: {
 				grey: '#9D9D9D',
 				lightGreen: '#D6FFC8',
+				lightRed: '#FEF0F0',
 				priceGrey: '#BDBDBD',
 			},
 		},
